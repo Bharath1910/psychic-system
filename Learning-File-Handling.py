99a9d467e4be5d0b2b566fd2387f12b410e44779
Bharath <av.bharathshanmugam@gmail.com>
2021-07-23 20:55:43 +0530

    hm, so im learning variable scopes

--- Learning-File-Handling.py
+++ Learning-File-Handling.py
@@ -1,3 +1,14 @@
-f = open("/home/bharath/Codes/Python/Projects/Python-Projects/exampletext.txt", 'r')
+def fun1():
+    x = 100
+    def fun2():
+        nonlocal x #change it to global or remove this declaration
+        x = 200
+    print("Before calling fun2: " + str(x))
+    print("Calling fun2 now:")
+    fun2()
+    print("After calling fun2: " + str(x))
 
-print(f.read())
+
+x=50
+fun1()
+print("x in main: " + str(x))
